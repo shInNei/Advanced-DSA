afd884cfced5c0764ca85940aa7f235660e281f9
Thịnh Nguyễn <109053741+shInNei@users.noreply.github.com>
2024-05-28 10:10:33 +0700

    Update cutRod.cpp

--- Dynamic Programming/Rod cutting/cutRod.cpp
+++ Dynamic Programming/Rod cutting/cutRod.cpp
@@ -1,1 +1,20 @@
-
+    int cutRod(int price[], int n) {
+        //code here
+        
+        // dp is an array of optimal revenue
+        vector<int> dp(n + 1, 0);
+        
+        /* If we want to calculate the optimal value of n,
+         * We must loop from 1 -> n.
+         * Then in each value, we simply calculate base on older optimal value
+         * == loop ( max(q, price[] + r[]))
+        */
+        for(int i = 1; i <= n; ++i) {
+            int q = INT_MIN;
+            for(int j = 0; j < i; ++j) {
+                q = max(q, price[j] + dp[i - j - 1]);
+            }
+            dp[i] = q;
+        }
+        return dp[n];
+    }
